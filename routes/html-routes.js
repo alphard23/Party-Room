7101ee4fb8b8a4b103f62c4ff98d8d08a197d949
eric2esca <eric2esca@gmail.com>
2018-03-23 09:14:39 -0500

    first youtube integration included. click on home button to access youtube navbar

---
+++ routes/html-routes.js
@@ -0,0 +1,24 @@
+var path = require("path");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+      app.get("/", function(req, res){
+        res.render("home");
+      });
+      
+      app.get("/allRooms", function(req, res){
+        res.render("allRooms");
+      });
+      
+      app.get("/songs", function(req, res){
+        res.render("songs");
+      });
+
+      app.get("/youtube", function(req, res){
+        res.sendFile(path.join(__dirname, "youtube.html"));
+      });
+  
+  };
+  

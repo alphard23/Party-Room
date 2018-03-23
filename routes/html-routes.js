ef25b452799c84e26fb0abe6bd04a0bc90022923
eric2esca <eric2esca@gmail.com>
2018-03-22 20:19:43 -0500

    update

---
+++ routes/html-routes.js
@@ -0,0 +1,20 @@
+var path = require("path");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+      app.get("/", function(req, res){
+        res.render("home");
+      });
+      
+      app.get("/allRooms", function(req, res){
+        res.render("allRooms");
+      });
+      
+      app.get("/songs", function(req, res){
+        res.render("songs");
+      });
+  
+  };
+  
